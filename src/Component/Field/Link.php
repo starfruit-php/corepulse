93779696b14d0903c9680b852ca7248d02208a33
git lab <gitlab@starfruit.com.vn>
2024-10-08 09:49:43 +0700

    update field link

--- src/Component/Field/Link.php
+++ src/Component/Field/Link.php
@@ -8,21 +8,41 @@ class Link extends Input
 {
     public function format($value)
     {
-        if ($value) {
-            $data = $value->getObjectVars();
-            $data['path'] = $value->getPath();
+        $data = [];
+        $params = [
+            'property' => [
+                'target', 'parameters', 'title', 'anchor'
+            ],
+            'advanced' => [
+                'accessKey', 'rel', 'tabIndex', 'class'
+            ]
+        ];
 
-            return $data;
+        foreach ($params as $paramKey => $paramValue) {
+            foreach ($paramValue as $param) {
+                $getFunc = 'get' . ucfirst($param);
+                $data[$paramKey][$param] = $value?->$getFunc();
+            }
         }
 
-        return null;
+        $data['path'] = $value?->getPath();
+        $data['text'] = $value?->getText();
+        // $data['linktype'] = $value?->getLinktype();
+        // if ($data['linktype'] == 'internal') {
+        //     $data['data']['internal'] = $value?->getInternal();
+        //     $data['data']['internalType'] = $value?->getInternalType();
+        // } elseif ($data['linktype'] == 'direct') {
+        //     $data['data']['direct'] = $value?->getDirect();
+        // }
+
+        return $data;
     }
 
     public function formatDataSave($value)
     {
         $link = new DataLink();
 
-        $fields = ['path', 'text', 'title', 'target', 'parameters', 'anchorLink', 'accessKey', 'rel', 'tabIndex', 'class'];
+        $fields = ['path', 'text', 'title', 'target', 'parameters', 'anchorLink', 'accessKey', 'rel', 'tabIndex', 'class', 'attributes'];
         foreach ($fields as $key) {
             if (isset($value[$key])) {
                 $func = 'set' . ucfirst( $key);
